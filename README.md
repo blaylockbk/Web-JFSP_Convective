f127e26479ec9c8f9cb39986458ede88ada78261
Brian Kenneth Blaylock <u0553130@ad.utah.edu>
2018-02-15 11:34:28 -0700

    updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Web-JFSP_Convective
 Webpage for JSFP convective outflow project at the University of Utah
+
+[JFSP Convective Outflow Project Page](http://meso1.chpc.utah.edu/jfsp_convective/)
--- js/siteclose.js
+++ js/siteclose.js
@@ -4,9 +4,8 @@ document.write('\
 <p style="text-align:center; font-family:Segoe UI; font-size:12px">\
 This page is maintained by Brian Blaylock and Taylor McCorkle &copy 2018, \
  <a href="http://www.atmos.utah.edu/" style="color:#990000; text-decoration:none">Department of Atmospheric Science, University of Utah</a>. \
- <a href="https://github.com/blaylockbk" style="color:#990000; text-decoration:none"><big><i class="fab fa-github fa-fw"></i></big> </a>\
- <a style="color:#990000; text-decoration:none; font-weight:700" href="mailto:brian.blaylock@utah.edu">\
-<big><i class="fas fa-envelope fa-fw"></i></big> brian.blaylock@utah.edu\
+ <a href="https://github.com/blaylockbk/Web-JFSP_Convective" style="color:#990000; text-decoration:none"><big><i class="fab fa-github fa-fw"></i></big> </a>\
+ <a style="color:#990000; text-decoration:none; font-weight:700" href="mailto:brian.blaylock@utah.edu"><big><i class="fas fa-envelope fa-fw"></i></big>\
 </a>\
 </p>\
 </div>\
